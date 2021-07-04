219c0ab13b9a72d19b705377458c32f055ebb978
Mr. Code Format <codeformat@connorcode.com>
2021-07-04 01:24:32 +0000

    💎 Automated Code Formatting

--- server.js
+++ server.js
@@ -4,5 +4,5 @@ const app = express();
 app.use(express.static('static'));
 
 app.listen(3000, () => {
-  console.log('Server listening on port 3000');
+    console.log('Server listening on port 3000');
 });
