cc5e9c215cbc8f4dab9fc939b6fef85e7657b37b
Connor Slade <connor@connorcode.com>
2021-07-05 22:06:15 -0400

    🏗 Rust unit tests

---
+++ test_test/src/main.rs
@@ -0,0 +1,16 @@
+fn main() {
+    println!("{}", greeter("Connor"));
+}
+
+fn greeter(name: &str) -> String{
+    return format!("Hello, {}!", name)
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    #[test]
+    fn test_greeter() {
+        assert_eq!("Hello, Connor!", greeter("Connor"));
+    }
+}
